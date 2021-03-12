7f1202ac20b4f39bd8ab7a1f65bc77d0da2e7bee
jem <michael.jerger@meissa-gmbh.de>
2021-03-12 16:03:06 +0100

    no code signing

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dda/mastodon-bot "1.10.5"
+(defproject dda/mastodon-bot "1.10.6"
   :description "Bot to publish twitter, tumblr or rss posts to an mastodon account."
   :url "https://github.com/yogthos/mastodon-bot"
   :author "Dmitri Sotnikov"
@@ -9,8 +9,8 @@
   :resource-paths ["src/main/resources"]
   :repositories [["snapshots" :clojars]
                  ["releases" :clojars]]
-  :deploy-repositories [["snapshots" :clojars]
-                        ["releases" :clojars]]
+  :deploy-repositories [["releases"  {:sign-releases false :url "https://clojars.org"}]
+                        ["snapshots" {:sign-releases false :url "https://clojars.org"}]]
   :profiles {:test {:test-paths ["src/test/cljc"]
                     :resource-paths ["src/test/resources"]
                     :dependencies []}})
